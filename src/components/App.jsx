89f4a84a0d844a6a96b61fd315bf15e4ebea797a
dzapeka <douson@gmail.com>
2023-09-26 02:29:00 +0200

    implement Transaction History component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import Profile from './Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 import user from 'user.json';
 import data from 'data.json';
 import friends from 'friends.json';
+import transactions from 'transactions.json';
 
 export const App = () => {
   return (
@@ -20,11 +22,15 @@ export const App = () => {
       </section>
       <section className="task-container">
         <h1>2- Statistics Section</h1>
-        <Statistics title="Upload stats" stats={data}></Statistics>
+        <Statistics title="Upload stats" stats={data} />
       </section>
       <section className="task-container">
         <h1>3 - Friends list</h1>
-        <FriendList friends={friends}></FriendList>
+        <FriendList friends={friends} />
+      </section>
+      <section className="task-container">
+        <h1>4 - Transaction History</h1>
+        <TransactionHistory items={transactions} />
       </section>
     </>
   );
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import styles from './TransactionHistory.module.css';
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={styles.TransactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ type, amount, currency, id }) => (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
